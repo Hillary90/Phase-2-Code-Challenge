2f2147ff27f4ac7b4385d96457a1d0596a11f9eb
Hillary Tanui <hillarytanui80@gmail.com>
2025-10-30 01:04:54 +0300

    added files

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), 
-    tailwindcss(),
-  
+    tailwindcss(), 
   ],
   server: {
     port: 3000
-  }
+  },
+  build: {
+    outDir: 'dist',
+  },
+  base: '/'
 })
+
+
